dd7e4a82fc1f0907fe4475f008773f32290bd144
Alexander Yugov <CaxaYou@yandex.ru>
2024-04-25 17:43:53 +0200

    Airflow dag decorators

--- dwh/airflow/dags/titanic_dag.py
+++ dwh/airflow/dags/titanic_dag.py
@@ -1,9 +1,7 @@
 import datetime as dt
 import logging
 
-from airflow.models import DAG
-from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+from airflow.decorators import dag, task
 
 from dwh.airflow.dags.config import DependencyConfig
 from dwh.core.domain.titanic import TitanicPassengersDownloadJob, calculate_sex_dm, create_titanic_table
@@ -16,68 +14,52 @@ args = {
 }
 
 
-def create_titanic_table_task():
-    logging.info("Creating titanic table")
-
-    create_titanic_table(DependencyConfig.db_connection())
-
-    logging.info("Titanic table created")
-
-
-def download_titanic_dataset_task():
-    logging.info("Downloading titanic dataset")
-
-    job = TitanicPassengersDownloadJob(
-        DependencyConfig.Connectors.titanic_passenger_connector(), DependencyConfig.Repository.titanic_passenger_repository()
-    )
-
-    job.download_titanic_dataset()
-
-    logging.info("Downloaded titanic dataset")
-
-
-def create_titanic_sex_dm_task():
-    logging.info("Calculating Sex DM")
-
-    calculate_sex_dm(DependencyConfig.db_connection())
-
-    logging.info("Sex DM created")
-
-
-dag = DAG(
-    dag_id="titanic_dag",
+@dag(
+    default_args=args,
     schedule_interval="0/15 * * * *",
-    start_date=dt.datetime(2024, 4, 1),
     catchup=False,
     tags=["demo", "stg", "cdm", "titanic"],
     is_paused_upon_creation=False,
-    default_args=args,
 )
+def titanic_dag():
+    @task()
+    def start_task():
+        logging.info("Here we start!")
+
+    @task()
+    def create_titanic_table_task():
+        logging.info("Creating titanic table")
+
+        create_titanic_table(DependencyConfig.db_connection())
+
+        logging.info("Titanic table created")
+
+    @task()
+    def download_titanic_dataset_task():
+        logging.info("Downloading titanic dataset")
+
+        job = TitanicPassengersDownloadJob(
+            DependencyConfig.Connectors.titanic_passenger_connector(), DependencyConfig.Repository.titanic_passenger_repository()
+        )
+
+        job.download_titanic_dataset()
+
+        logging.info("Downloaded titanic dataset")
+
+    @task()
+    def create_titanic_sex_dm_task():
+        logging.info("Calculating Sex DM")
+
+        calculate_sex_dm(DependencyConfig.db_connection())
+
+        logging.info("Sex DM created")
+
+    start = start_task()
+    create_table = create_titanic_table_task()
+    download_titanic_dataset = download_titanic_dataset_task()
+    create_titanic_sex_dm = create_titanic_sex_dm_task()
+
+    start >> create_table >> download_titanic_dataset >> create_titanic_sex_dm  # type: ignore
 
 
-start = BashOperator(
-    task_id="start",
-    bash_command='echo "Here we start! "',
-    dag=dag,
-)
-
-titanic_sex_dm = PythonOperator(
-    task_id="create_titanic_table",
-    python_callable=create_titanic_table_task,
-    dag=dag,
-)
-
-create_titanic_dataset = PythonOperator(
-    task_id="download_titanic_dataset",
-    python_callable=download_titanic_dataset_task,
-    dag=dag,
-)
-
-titanic_sex_dm = PythonOperator(
-    task_id="create_titanic_sex_dm",
-    python_callable=create_titanic_sex_dm_task,
-    dag=dag,
-)
-
-
-start >> create_titanic_dataset >> titanic_sex_dm
+titanic_dag()
