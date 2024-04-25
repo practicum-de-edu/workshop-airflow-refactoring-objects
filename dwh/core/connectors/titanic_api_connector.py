214c8ffe7737be60c59d0451d29c6f1a7e57924f
Alexander Yugov <CaxaYou@yandex.ru>
2024-04-25 16:47:19 +0200

    parsing of response via pydantic

--- dwh/core/connectors/titanic_api_connector.py
+++ dwh/core/connectors/titanic_api_connector.py
@@ -1,10 +1,10 @@
 import csv
 from abc import ABC, abstractmethod
 from logging import getLogger
+from typing import Iterable
 
 import requests
 
-from dwh.core.domain.entities.gender import Gender
 from dwh.core.domain.entities.passenger import Passenger
 
 logger = getLogger(__name__)
@@ -20,15 +20,6 @@ class TitanicConnector(ITitanicApiConnector):
     def __init__(self, url: str):
         self.url = url
 
-    def resolve_gender(self, sex: str) -> Gender:
-        if sex == "male":
-            return Gender.MALE
-
-        if sex == "female":
-            return Gender.FEMALE
-
-        raise ValueError(f"sex {sex} is not recognized.")
-
     def download_titanic_dataset(self) -> list[Passenger]:
         logger.info("Downloading titanic dataset")
 
@@ -37,22 +28,9 @@ class TitanicConnector(ITitanicApiConnector):
 
         decoded_content = download.content.decode("utf-8")
 
-        cr = csv.reader(decoded_content.splitlines(), delimiter=",")
-        my_list = list(cr)
+        passenger_reader: Iterable[dict] = csv.DictReader(decoded_content.splitlines(), delimiter=",")
 
-        passengers = [
-            Passenger(
-                survived=bool(row[0]),
-                p_class=int(row[1]),
-                name=row[2],
-                gender=self.resolve_gender(row[3]),
-                age=float(row[4]),
-                siblings_spouses_aboard=int(row[5]),
-                parents_children_aboard=int(row[6]),
-                fare=float(row[7]),
-            )
-            for row in my_list[1:]
-        ]
+        passengers = [Passenger(**p) for p in passenger_reader]
 
         logger.info(f"Downloaded titanic dataset, len: {len(passengers)}.")
         return passengers
--- dwh/core/domain/entities/passenger.py
+++ dwh/core/domain/entities/passenger.py
@@ -1,14 +1,25 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 
 from dwh.core.domain.entities.gender import Gender
 
 
 class Passenger(BaseModel):
-    age: float = Field()
-    fare: float = Field()
-    name: str = Field()
-    p_class: int = Field()
-    parents_children_aboard: int = Field()
-    gender: Gender = Field()
-    siblings_spouses_aboard: int = Field()
-    survived: bool = Field()
+    age: float = Field(alias="Age")
+    fare: float = Field(alias="Fare")
+    name: str = Field(alias="Name")
+    p_class: int = Field(alias="Pclass")
+    parents_children_aboard: int = Field(alias="Parents/Children Aboard")
+    gender: Gender = Field(alias="Sex")
+    siblings_spouses_aboard: int = Field(alias="Siblings/Spouses Aboard")
+    survived: bool = Field(alias="Survived")
+
+    @field_validator("gender", mode="before")
+    @classmethod
+    def decode_gender(cls, obj: str) -> Gender:
+        if obj == "male":
+            return Gender.MALE
+
+        if obj == "female":
+            return Gender.FEMALE
+
+        raise ValueError(f"sex {obj} is not recognized.")
